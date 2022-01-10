2c2c303138d1b5b071fff1096b31042239a2b796
vaishalinerkar1987 <suverna.1288@gmail.com>
2022-01-10 12:08:53 +0100

    chapter1exercise

---
+++ chapter1exercise.py
@@ -0,0 +1,15 @@
+#exercise chapter 1
+number = 4 * 6
+print(number)
+
+number =10+20
+print(number)
+
+number = 30 / 5
+print(number)
+
+integer = 47
+print(integer)
+
+
+print(47)
--- forin.py
+++ forin.py
@@ -37,3 +37,20 @@ subjects = {
 }
 subsubjects = "english"
 print(subsubjects , "says:" , subjects[subsubjects])
+
+
+#exercise chapter 1
+number = 4 * 6
+print(number)
+
+number =10+20
+print(number)
+
+number = 30 / 5
+print(number)
+
+integer = 47
+print(integer)
+
+
+print(47)
