3363ae2ed1cdb9faa986cecf684c61cf9344e117
vaishalinerkar1987 <suverna.1288@gmail.com>
2022-01-11 17:32:07 +0100

    chapter1exercise and chapter 2 programs

--- chapter2exercise.py
+++ chapter2exercise.py
@@ -17,3 +17,59 @@ print(type(5 + 2.0))
 variable = 5j, 3+9j
 print(variable)
 
+########### third chapter ######
+bool(True)
+print(bool(True))
+
+bool(1)
+print(bool(1))
+
+type(23)
+print(type(23))
+
+bool(-41.0)
+print(bool(-41.0))
+
+bool(0)
+print(bool(0))
+
+bool(0.0)
+print(bool(0.0))
+
+bool(False)
+print(bool(False))
+
+###  digits in python represents literal integer
+5
+print(5)
+
+123
+print(123)
+
+-123
+print(-123)
+
++123
+print(+123)
+
+1,000,000
+print(1,00,00,000)
+
+milion = 1,000,23,20
+print(milion)
+
+milion = 1_000_00_20_003_40
+print(milion)
+
+1_2_3
+print(1_2_3)
+
+numbers=4*5
+print(numbers)
+
+
+a=25
+print(a)
+a= a -2
+print(a)
+
