038b10d48906cc5a768988e655fab857b18bc170
vaishalinerkar1987 <suverna.1288@gmail.com>
2022-01-11 16:43:16 +0100

    chapter1exercise and chapter 2 programs

--- chapter1exercise.py
+++ chapter1exercise.py
@@ -1,8 +1,8 @@
-#exercise chapter 1
+# exercise chapter 1
 number = 4 * 6
 print(number)
 
-number =10+20
+number = 10 + 20
 print(number)
 
 number = 30 / 5
@@ -13,31 +13,117 @@ print(integer)
 
 print(47)
 
-#addition program in python using variable and assigning values
+##########################################second chapter starts##########
+# addition program in python using variable and assigning values
 
-a=10
-b=30
-y=a+b
-print("answer is:" , y)
+a = 10
+b = 30
+y = a + b
+print("answer is:", y)
 
 ####
-a=15
-c=a+20
+a = 15
+c = a + 20
 print("answer is: ", c)
 ###############in algebbra we would do in backward direction
-#assign value to y
+# assign value to y
 y = 10
-#to calculate x
+# to calculate x
 x = 12 - y
-print("answer is: " , x)
+print("answer is: ", x)
 
 ###############
-#assign value 10 to the name a
-a=10
+# assign value 10 to the name a
+a = 10
 print(a)
-#assign value a to name(variable) b
+# assign value a to name(variable) b
 b = a
 print(b)
+############type of using variables and without variables in data types second chap
+number = 12
+print(type(number))
+
+number = '13'
+print(type(number))
 
 
+type(7)
+print(type(7))
 
+type('34')
+print(type('34'))
+
+type('vaishu')
+print(type('vaishu'))
+
+type(98.8)
+print(type(98.8))
+########type of using variables
+number = 98.2
+print(type(number))
+
+value = 'vaishali'
+print(type(value))
+
+#########little complex
+a =38
+b =a
+print(a)
+print(type(a))
+print(type(b))
+type(58)
+print(type(58))
+type(89.7)
+print(type(89.7))
+type('23')
+print(type('23'))
+type('abcd')
+print(type('abcd'))
+
+############array consist of list of items is in python list
+a = [2,3,4,'abc']
+print(a)
+print(type(a))
+#########lots of items in object is dictionary
+b = {'name': 'vaishali' ,
+     'age': 34 ,
+      'gender': 'female'}
+print(b)
+print(type(b))
+###### set
+c=set([2 ,3, 4])
+print(c)
+print(type(c))
+######## tuple
+d=(2,3,4)
+print(d)
+print(type(d))
+
+e = b'ab\xff'
+print(e)
+print(type(e))
+
+#######assig value 2 to more than one variable
+ek = eins =one= 2
+print("answer is: ", ek)
+print('the second variable  answer is: ',eins )
+print("the third variable ans is: " , one)
+
+##########
+x=5
+print(x)
+y = x
+print(y)
+
+x =29
+print(x)
+print(y)
+
+########list is a mutable array of value
+a = [5 , 6, 8,9]
+print("list of array value is",a)
+b=a
+print(b)
+a[1] = 99
+print(a)
+print(b)
---
+++ chapter2exercise.py
@@ -0,0 +1,19 @@
+###### first
+prince = 99
+print(prince)
+
+####second
+type(5)
+print(type(5))
+
+###### third
+type(3.0)
+print(type(3.0))
+
+########## fourth
+type(5 + 2.0)
+print(type(5 + 2.0))
+
+variable = 5j, 3+9j
+print(variable)
+
