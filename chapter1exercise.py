084b4a705b185932515b7eb2773620acb67ed7b7
vaishalinerkar1987 <suverna.1288@gmail.com>
2022-01-11 12:58:44 +0100

    chapter1exercise

--- chapter1exercise.py
+++ chapter1exercise.py
@@ -11,5 +11,33 @@ print(number)
 integer = 47
 print(integer)
 
-
 print(47)
+
+#addition program in python using variable and assigning values
+
+a=10
+b=30
+y=a+b
+print("answer is:" , y)
+
+####
+a=15
+c=a+20
+print("answer is: ", c)
+###############in algebbra we would do in backward direction
+#assign value to y
+y = 10
+#to calculate x
+x = 12 - y
+print("answer is: " , x)
+
+###############
+#assign value 10 to the name a
+a=10
+print(a)
+#assign value a to name(variable) b
+b = a
+print(b)
+
+
+
