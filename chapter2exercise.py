8796b9451166c4db276f3988c00e6c87f88833a1
vaishalinerkar1987 <suverna.1288@gmail.com>
2022-01-13 12:46:47 +0100

    chapter1exercise and chapter 2 programs and continue

--- chapter2exercise.py
+++ chapter2exercise.py
@@ -64,6 +64,9 @@ print(milion)
 1_2_3
 print(1_2_3)
 
+a=1_2_3
+print(a)
+
 numbers=4*5
 print(numbers)
 
@@ -73,3 +76,78 @@ print(a)
 a= a -2
 print(a)
 
+5+9
+print(5+9)  # or
+a=5+9
+print(a)
+
+b=4-10
+print(b)
+
+c = 5+5+4    +6
+print(c)
+
+mul = 5*6
+print(mul)
+
+# / carries out floating point result division
+#if we are divide integer by integer
+
+9/5
+print(9/5)  # or
+integer = 9/5
+print(integer)
+
+# // carries out integer division
+9//5
+print(9//5) # or
+integer = 9//5
+print(integer)
+
+#####
+a =50
+print(a)
+b=a-3
+print(b)
+
+####    combined operators
+a=28
+a -= 3
+print("combine operators with assignment: " , a)
+
+b=30
+b +=30
+print("combined arithmatic operators: " , b)
+
+c=4
+c *=5
+print(c)
+
+d=100
+d /= 20
+print(d)
+
+d=50
+d //=20  ##truncating integer division
+print(d)
+
+e = 9%5
+print("producing remainder is: " , e)
+#to get both quotient and remainder(function named divmod is given to the integers 9 and 5 and it returns tuple in parenthesis seperated by comma)
+divmod(9,5)
+print(divmod(9,5)) # ans is (1,4---1 is quotient and 4 as remainder)
+
+divmod(40,10)
+print(divmod(40,10))
+
+### exponential
+a= 3**3.0   # (3 raise to 3 is 3*3*3=27)
+print('exponential ans is: ' ,a)
+b= 1.2 **3
+print(b)
+
+b =3+(2*4)    # 3+2*4
+print(b)
+
+c= (-5) ** 2
+print(c)
