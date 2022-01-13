5d220c3ee7ddcde6aa2b1c76e81604880f653f99
vaishalinerkar1987 <suverna.1288@gmail.com>
2022-01-13 16:33:54 +0100

    chapter3exercise completed

---
+++ chapter3exercise.py
@@ -0,0 +1,32 @@
+### first exercise
+### 1 min = 60 sec
+   # 60 min = 60 *60=3600
+ #seconds per hr= 60 *60
+ ### 1 hr = 60 min.
+
+#how many seconds are in hour
+seconds_in_an_hour = 60*60
+print("seconds in an hour: " ,seconds_in_an_hour)
+
+### second
+seconds_per_hour= 60*60
+print(seconds_per_hour)
+
+### third
+#how many seconds are in a day
+seconds_per_hour= seconds_per_hour * 24
+print("seconds are in a day: " , seconds_per_hour)
+
+## fourth
+seconds_per_day= 24 * seconds_per_hour
+print("seconds in a 24 days: " , seconds_per_day)
+
+# fifth (divide using / floating) will get answer in decimal
+c = seconds_per_day / seconds_per_hour
+print(c)
+
+# six (divide seconds_per_day/seconds_per_hour) using // ans in non decimal
+
+d = seconds_per_day // seconds_per_hour
+print(d)
+
