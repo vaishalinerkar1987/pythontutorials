adb05437402be555c5d8cbf605ea9433f08fd5ae
vaishalinerkar1987 <suverna.1288@gmail.com>
2022-01-18 17:27:01 +0100

    chapter 4 exercises

---
+++ chapter4exercise.py
@@ -0,0 +1,47 @@
+####### 1 exercise
+# choose number between 1 and 10 and assign it to the variable secret
+secret= 5
+# choose another number between 1 and 10 and assign it to the variable guess
+guess=7
+# write conditional test (if , elif, else)
+# if guess is less than secret to print 'too low'
+if guess < secret:
+    print(guess , "is to low")
+elif guess > secret:
+    print("to high")
+else:
+    print("just right")
+
+
+#### second exercise
+
+# choose number between 1 and 10 and assign it to the variable secret
+secret= 7
+# choose another number between 1 and 10 and assign it to the variable guess
+guess=7
+# write conditional test (if , elif, else)
+# if guess is less than secret to print 'too low'
+if guess < secret:
+    print(guess , "is to low")
+elif guess > secret:
+    print("to high")
+else:
+    print("just right")
+
+     ## third exercise
+    #Assign True or False to the variables small and green. \
+    # Write some if/else statements to \
+    # print which of these matches those choices: cherry, pea, watermelon, pumpkin.
+small=False
+green=True
+#write if else statements
+if small:
+    if green:
+        print("cherry")
+    else:
+        print("pea")
+else:
+    if green:
+        print("watermelon")
+    else:
+        print("pumpkin")
